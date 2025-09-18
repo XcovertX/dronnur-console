3480ea77aa970a2fc51e90419fcdb2044b613aa8
James <james@ammortal.com>
2025-09-18 15:15:07 -0400

    remove unused import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import DronnurConsoleWireframe from "@/components/DronnurConsoleWireframe";
-import DronnurConsole from "@/components/DronnurConsole";
 
 export default function Home() {
-  return <DronnurConsole />;
+  return <DronnurConsoleWireframe />;
 }
